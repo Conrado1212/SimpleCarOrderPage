d3a9525b1e58c18377162b8a2f8f3bbb275bb89e
Conrado1212 <conrado1212@gmail.com>
2022-05-20 17:40:18 +0200

    menu in php ready

--- admin/index.php
+++ admin/index.php
@@ -1,52 +1,5 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/admin.css">
-    <link href='http://fonts.googleapis.com/css?family=Lato:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <link href="../css/fontello.css" rel="stylesheet" type="text/css" />
-    <title>Admin dashboard</title>
-</head>
-<body>
-    <div class="nav">
-    <nav>
-        <div class="conteiner">
-       <div class="logo">
-           <img src="../img/logo_ccexpress.png" alt="Car logo" class="img-resp">
-           <span style="color: #808080">Electric</span>OrderApp.com
-       </div>
-       <div class="menu">
-       
-            <ul>
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a href="types.html">Admin</a>
-                </li>
-                <li>
-                    <a href="cars.html">Category</a>
-                </li>
-                <li>
-                    <a href="cars.html">Cars</a>
-                </li>
-                <li>
-                    <a href="buy.html">Buy</a>
-                </li>
-            </ul>
-       </div>
-       
-    
-       <div class="clear">
+<?php include('part/menu.php'); ?>
 
-       </div>
-       <div class="contact">
-
-       </div>
-    </nav>
-</div>
-</div>
 <section class="categories">
     <div class="conteiner">
         <div class="button-box">
